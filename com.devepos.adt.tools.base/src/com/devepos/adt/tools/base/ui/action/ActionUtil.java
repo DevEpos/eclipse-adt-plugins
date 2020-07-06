aca63041498d71b92e6a909d2d11849524f1972b
stockbal <ludwig.stockbauer-muhr@msg.group>
2020-07-06 20:58:48 +0200

    feat: new method to create Action with Data

--- com.devepos.adt.tools.base/src/com/devepos/adt/tools/base/ui/action/ActionUtil.java
+++ com.devepos.adt.tools.base/src/com/devepos/adt/tools/base/ui/action/ActionUtil.java
@@ -20,6 +20,11 @@ public class ActionUtil {
 		void execute();
 	}
 
+	@FunctionalInterface
+	public interface IActionRunWithData<T> {
+		void execute(T data);
+	}
+
 	/**
 	 * Create anonymous {@link Action} class with the given {@link Consumer
 	 * runConsumer} as the implementation for the {@link Action#run()} method
@@ -38,6 +43,30 @@ public class ActionUtil {
 			@Override
 			public void run() {
 				actionRunner.execute();
+			}
+		};
+	}
+
+	/**
+	 * Create anonymous {@link Action} class with the given {@link Consumer
+	 * runConsumer} as the implementation for the {@link Action#run()} method.
+	 * 
+	 * @param  text            the text/tooltip for the action
+	 * @param  imageDescriptor the optional image descriptor for the action
+	 * @param  actionRunner    the implementation for the {@link Action#run()}
+	 *                         method
+	 * @param  actionData      the actual data that will be handled during the
+	 *                         execution of the passed {@code actionRunner}
+	 * @return                 the created Action instance
+	 */
+	public static <T> Action createActionWithData(final String text, final ImageDescriptor imageDescriptor,
+		final IActionRunWithData<T> actionRunner, final T actionData) {
+		Assert.isNotNull(actionRunner, "Parameter 'actionRunner' must not be null");
+
+		return new Action(text, imageDescriptor) {
+			@Override
+			public void run() {
+				actionRunner.execute(actionData);
 			}
 		};
 	}
