99aac29db03969be82cc4649aa2ed695ebc6fcf1
stockbal <ludwig.stockbauer-muhr@msg.group>
2020-10-03 17:25:26 +0200

    refactor: use mouse listener static instead of adapter

--- com.devepos.adt.tools.base.ui/src/com/devepos/adt/tools/base/ui/celleditor/ExtendedDialogCellEditor.java
+++ com.devepos.adt.tools.base.ui/src/com/devepos/adt/tools/base/ui/celleditor/ExtendedDialogCellEditor.java
@@ -2,8 +2,7 @@ package com.devepos.adt.tools.base.ui.celleditor;
 
 import org.eclipse.jface.viewers.DialogCellEditor;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MouseAdapter;
-import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -21,14 +20,11 @@ public abstract class ExtendedDialogCellEditor extends DialogCellEditor {
 	@Override
 	protected Control createContents(final Composite cell) {
 		final Control control = super.createContents(cell);
-		control.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseUp(final MouseEvent event) {
-				if (ExtendedDialogCellEditor.this.button != null) {
-					ExtendedDialogCellEditor.this.button.notifyListeners(SWT.Selection, null);
-				}
+		control.addMouseListener(MouseListener.mouseUpAdapter(e -> {
+			if (this.button != null) {
+				this.button.notifyListeners(SWT.Selection, null);
 			}
-		});
+		}));
 		return control;
 	}
 
