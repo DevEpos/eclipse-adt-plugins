19f4b1ee5c020457de37efcaa7e9d6b4eb333d86
stockbal <stocki87@gmail.com>
2021-04-10 17:52:04 +0200

    refactor: use icons from existing plugins

--- com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/internal/AdtBaseUIPlugin.java
+++ com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/internal/AdtBaseUIPlugin.java
@@ -49,23 +49,30 @@ public class AdtBaseUIPlugin extends AbstractAdtUIPlugin {
     protected void initializeImageRegistry(final ImageRegistry imageRegistry) {
         registerImage(imageRegistry, IAdtBaseImages.ARROW_DOWN, "icons/ArrowDown.png");
         registerImage(imageRegistry, IAdtBaseImages.ARROW_UP, "icons/ArrowUp.png");
-        registerImage(imageRegistry, IAdtBaseImages.REFRESH, "icons/Refresh.png");
-        registerImage(imageRegistry, IAdtBaseImages.IMPORT, "icons/ImportWizard.png");
-        registerImage(imageRegistry, IAdtBaseImages.EXPORT, "icons/ExportWizard.png");
-        registerImage(imageRegistry, IAdtBaseImages.EXPAND_ALL, "icons/ExpandAll.png");
-        registerImage(imageRegistry, IAdtBaseImages.COLLAPSE_ALL, "icons/CollapseAll.png");
+        registerImage(imageRegistry, IAdtBaseImages.REFRESH, "icons/full/elcl16/refresh.png", "org.eclipse.search");
+        registerImage(imageRegistry, IAdtBaseImages.EXPORT, "icons/full/etool16/export_wiz.png", "org.eclipse.ui.ide");
+        registerImage(imageRegistry, IAdtBaseImages.IMPORT, "icons/full/etool16/import_wiz.png", "org.eclipse.ui.ide");
+        registerImage(imageRegistry, IAdtBaseImages.EXPAND_ALL, "icons/full/elcl16/expandall.png",
+            "org.eclipse.search");
+        registerImage(imageRegistry, IAdtBaseImages.COLLAPSE_ALL, "icons/full/elcl16/collapseall.png",
+            "org.eclipse.search");
         registerImage(imageRegistry, IAdtBaseImages.WAITING_INDICATOR, "icons/WaitingIndicator.png");
         registerImage(imageRegistry, IAdtBaseImages.EDIT_ACTION, "icons/EditMode.png");
         registerImage(imageRegistry, IAdtBaseImages.UNLOCK_ACTION, "icons/Unlock.png");
         registerImage(imageRegistry, IAdtBaseImages.SAP_GUI_OBJECT, "icons/SAPGUIObject.png");
         registerImage(imageRegistry, IAdtBaseImages.DATA_PREVIEW, "icons/DataPreview.png");
-        registerImage(imageRegistry, IAdtBaseImages.WHERE_USED_LIST, "icons/WhereUsed.png");
-        registerImage(imageRegistry, IAdtBaseImages.SEARCH, "icons/Search.png");
-        registerImage(imageRegistry, IAdtBaseImages.RUN_OBJECT, "icons/RunObject.png");
+        registerImage(imageRegistry, IAdtBaseImages.WHERE_USED_LIST, "icons/etool/where_used.png",
+            "com.sap.adt.ris.whereused.ui");
+        registerImage(imageRegistry, IAdtBaseImages.SEARCH, "icons/full/etool16/search.png", "org.eclipse.search");
+        registerImage(imageRegistry, IAdtBaseImages.RUN_OBJECT, "icons/full/obj16/lrun_obj.png",
+            "org.eclipse.debug.ui");
         registerImage(imageRegistry, IAdtBaseImages.OTHER_OBJECT, "icons/OtherObject.png");
-        registerImage(imageRegistry, IAdtBaseImages.AUTOMATIC_LAYOUT, "icons/AutomaticOrientation.png");
-        registerImage(imageRegistry, IAdtBaseImages.HORIZONTAL_LAYOUT, "icons/HorizontalOrientation.png");
-        registerImage(imageRegistry, IAdtBaseImages.VERTICAL_LAYOUT, "icons/VerticalOrientation.png");
+        registerImage(imageRegistry, IAdtBaseImages.AUTOMATIC_LAYOUT, "icons/full/elcl16/automaticOrientation.png",
+            "org.eclipse.search");
+        registerImage(imageRegistry, IAdtBaseImages.HORIZONTAL_LAYOUT, "icons/full/elcl16/horizontalOrientation.png",
+            "org.eclipse.search");
+        registerImage(imageRegistry, IAdtBaseImages.VERTICAL_LAYOUT, "icons/full/elcl16/verticalOrientation.png",
+            "org.eclipse.search");
         registerImage(imageRegistry, IAdtBaseImages.TRANSPORT, "icons/Transport.png");
         registerImage(imageRegistry, IAdtBaseImages.SHARE, "icons/Share.png");
         registerImage(imageRegistry, IAdtBaseImages.SHARE_OVR, "icons/ovr/Share.png");
