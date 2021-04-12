ef12bf690464d6180554e2ee19012bed219fe942
stockbal <stocki87@gmail.com>
2021-04-12 11:28:55 +0200

    feat: create utility for some common workbench stuff

---
+++ com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/util/WorkbenchUtil.java
@@ -0,0 +1,46 @@
+package com.devepos.adt.base.ui.util;
+
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * Utility handling some common workbench stuff
+ * 
+ * @author stockbal
+ *
+ */
+public class WorkbenchUtil {
+    /**
+     * Brings the given part to the top of the active page, but only if the
+     * {@code part} is visible in the active page
+     * 
+     * @param part a workbench part
+     */
+    public static void bringPartToFront(IWorkbenchPart part) {
+        if (part == null) {
+            return;
+        }
+        IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        if (activePage == null) {
+            return;
+        }
+        if (activePage.isPartVisible(part)) {
+            activePage.bringToTop(part);
+        }
+    }
+
+    /**
+     * Brings the active part of the active page to the front
+     */
+    public static void bringActivePartToFront() {
+        IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        if (activePage == null) {
+            return;
+        }
+        IWorkbenchPart activePart = activePage.getActivePart();
+        if (activePart != null && activePage.isPartVisible(activePart)) {
+            activePage.bringToTop(activePart);
+        }
+    }
+}
