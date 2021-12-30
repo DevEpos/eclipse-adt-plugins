7b549fed077adb8abc803113e0f7289fcee4ddc0
stockbal <stocki87@gmail.com>
2021-12-30 12:01:02 +0100

    refactor: replace current validation logic
    - use custom validation / model update logic
    
    Closes #1

--- com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/project/ProjectInput.java
+++ com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/project/ProjectInput.java
@@ -1,60 +1,171 @@
 package com.devepos.adt.base.ui.project;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.eclipse.swt.events.SelectionListener.widgetSelectedAdapter;
 
-import org.eclipse.core.databinding.DataBindingContext;
-import org.eclipse.core.databinding.UpdateValueStrategy;
-import org.eclipse.core.databinding.beans.typed.PojoProperties;
-import org.eclipse.core.databinding.conversion.IConverter;
-import org.eclipse.core.databinding.observable.value.IObservableValue;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.core.databinding.validation.IValidator;
-import org.eclipse.core.databinding.validation.ValidationStatus;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.databinding.swt.typed.WidgetProperties;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
+import com.devepos.adt.base.IStatusChangeListener;
 import com.devepos.adt.base.ui.internal.AdtBaseUIPlugin;
 import com.devepos.adt.base.ui.internal.messages.Messages;
 import com.devepos.adt.base.util.StringUtil;
 import com.sap.adt.tools.core.ui.AbapProjectProposalProvider;
 import com.sap.adt.tools.core.ui.dialogs.AbapProjectSelectionDialog;
-import com.sap.adt.util.ui.SWTUtil;
+import com.sap.adt.util.ui.swt.AdtSWTUtilFactory;
+import com.sap.adt.util.ui.swt.IAdtSWTUtil;
 
 /**
  * Input composite for entering the name of an ABAP Project
  *
- * @author stockbal
+ * @author Ludwig Stockbauer-Muhr
  */
+@SuppressWarnings("restriction")
 public class ProjectInput {
 
   private final IAbapProjectProvider projectProvider;
   private Text projectField;
-  private DataBindingContext dbc;
-  private final List<IValidator<IProject>> projectValidators = new ArrayList<>();
+  private IProject project;
+  private final Set<IValidator<IProject>> projectValidators = new HashSet<>();
+  private final Set<IStatusChangeListener> statusChangeListeners = new HashSet<>();
+  private boolean ensureLoggedOn;
 
+  /**
+   * Creates new project input
+   */
   public ProjectInput() {
-    this(new AbapProjectProxy(null));
+    this(new AbapProjectProxy(null), false);
   }
 
-  public ProjectInput(final IAbapProjectProvider projectProvider) {
+  /**
+   * Creates new project input
+   * 
+   * @param ensureLoggedOn ensures that the logged on status will also be checked
+   *                       if a valid project was chosen
+   * 
+   */
+  public ProjectInput(boolean ensureLoggedOn) {
+    this(new AbapProjectProxy(null), ensureLoggedOn);
+  }
+
+  /**
+   * Creates new project input
+   * 
+   * @param projectProvider ABAP project provider which will be updated from the
+   *                        text input of this control
+   * @param ensureLoggedOn  ensures that the logged on status will also be checked
+   *                        if a valid project was chosen
+   */
+  public ProjectInput(final IAbapProjectProvider projectProvider, boolean ensureLoggedOn) {
+    if (projectProvider == null) {
+      throw new IllegalArgumentException("Parameter 'projectProvider' cannot be null!");
+    }
     this.projectProvider = projectProvider;
-
+    this.ensureLoggedOn = ensureLoggedOn;
   }
 
+  /**
+   * Adds the given validator to the list of project validators
+   *
+   * @param validator validator which is to be used for project validation
+   */
+  public void addProjectValidator(final IValidator<IProject> validator) {
+    projectValidators.add(validator);
+  }
+
+  /**
+   * Adds the given status change listener
+   * 
+   * @param l the listener to be added
+   */
+  public void addStatusChangeListener(IStatusChangeListener l) {
+    statusChangeListeners.add(l);
+  }
+
+  /**
+   * Creates the controls for the project input
+   *
+   * @param parent the parent composite
+   */
+  public void createControl(final Composite parent) {
+    IAdtSWTUtil swtUtil = AdtSWTUtilFactory.getOrCreateSWTUtil();
+    final Composite projectInputRoot = new Composite(parent, SWT.NONE);
+    GridLayoutFactory.swtDefaults().numColumns(3).applyTo(projectInputRoot);
+    GridDataFactory.fillDefaults()
+        .align(SWT.FILL, SWT.FILL)
+        .grab(true, false)
+        .applyTo(projectInputRoot);
+
+    final Label projectInputLabel = new Label(projectInputRoot, SWT.NONE);
+    GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).applyTo(projectInputLabel);
+    projectInputLabel.setText(Messages.ProjectInput_Project_xfld);
+    swtUtil.setMandatory(projectInputLabel, true);
+
+    projectField = new Text(projectInputRoot, SWT.BORDER);
+    GridDataFactory.fillDefaults()
+        .align(SWT.FILL, SWT.CENTER)
+        .grab(true, false)
+        .applyTo(projectField);
+    // register project proposal provider
+    new AbapProjectProposalProvider(projectField);
+    swtUtil.addTextEditMenu(projectField);
+
+    projectField.addModifyListener(l -> {
+      IStatus projectStatus = validateProject(projectField.getText());
+      this.projectProvider.setProject(project);
+      fireStatusChange(projectStatus);
+    });
+
+    final Button projectBrowseButton = new Button(projectInputRoot, SWT.PUSH);
+    projectBrowseButton.setText(Messages.ProjectInput_BrowseProjects_xbut);
+    GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).applyTo(projectBrowseButton);
+    swtUtil.setButtonWidthHint(projectBrowseButton);
+    projectBrowseButton.addSelectionListener(widgetSelectedAdapter(e -> {
+      final IProject project = projectProvider.getProject();
+      final IProject resultProject = AbapProjectSelectionDialog.open(parent.getShell(), project);
+      if (resultProject != null) {
+        projectField.setText(resultProject.getName());
+      }
+    }));
+  }
+
+  /**
+   * Retrieves the project provider connected to the project input
+   * 
+   * @return the project provider connected to the project input
+   */
   public IAbapProjectProvider getProjectProvider() {
     return projectProvider;
+  }
+
+  /**
+   * Removes the given project validator
+   * 
+   * @param v the validator to be removed
+   */
+  public void removeProjectValidator(IValidator<IProject> v) {
+    projectValidators.remove(v);
+  }
+
+  /**
+   * Removes the given status change listener
+   * 
+   * @param l the listener to be removed
+   */
+  public void removeStatusChangeListener(IStatusChangeListener l) {
+    statusChangeListeners.remove(l);
   }
 
   /**
@@ -69,142 +180,58 @@ public class ProjectInput {
     }
   }
 
-  /**
-   * Creates the controls for the project input
-   *
-   * @param parent the parent composite
-   */
-  public void createControl(final Composite parent) {
-    final Composite projectInputRoot = new Composite(parent, SWT.NONE);
-    GridLayoutFactory.swtDefaults().numColumns(3).applyTo(projectInputRoot);
-    GridDataFactory.fillDefaults()
-        .align(SWT.FILL, SWT.FILL)
-        .grab(true, false)
-        .applyTo(projectInputRoot);
-
-    final Label projectInputLabel = new Label(projectInputRoot, SWT.NONE);
-    GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).applyTo(projectInputLabel);
-    projectInputLabel.setText(Messages.ProjectInput_Project_xfld);
-    SWTUtil.setMandatory(projectInputLabel, true);
-
-    projectField = new Text(projectInputRoot, SWT.BORDER);
-    GridDataFactory.fillDefaults()
-        .align(SWT.FILL, SWT.CENTER)
-        .grab(true, false)
-        .applyTo(projectField);
-    // register project proposal provider
-    new AbapProjectProposalProvider(projectField);
-    SWTUtil.addTextEditMenu(projectField);
-
-    final Button projectBrowseButton = new Button(projectInputRoot, SWT.PUSH);
-    projectBrowseButton.setText(Messages.ProjectInput_BrowseProjects_xbut);
-    GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).applyTo(projectBrowseButton);
-    SWTUtil.setButtonWidthHint(projectBrowseButton);
-    projectBrowseButton.addSelectionListener(new SelectionAdapter() {
-      @Override
-      public void widgetSelected(final SelectionEvent e) {
-        final IProject project = projectProvider.getProject();
-        final IProject resultProject = AbapProjectSelectionDialog.open(parent.getShell(), project);
-        if (resultProject != null) {
-          projectField.setText(resultProject.getName());
-        }
+  private IProject convertToProject(String projectName) {
+    // check if there is an ABAP project which matches the entered name
+    final IProject[] abapProjects = ProjectUtil.getAbapProjects();
+    String availableProjectName = null;
+    for (final IProject project : abapProjects) {
+      if (project.getName().equalsIgnoreCase(projectName)) {
+        availableProjectName = project.getName();
+        break;
       }
-    });
-    parent.addDisposeListener(e -> {
-      if (dbc != null) {
-        dbc.dispose();
-      }
-    });
-  }
-
-  /**
-   * Creates necessary bindings in the input
-   *
-   * @return the created context with the bindings
-   */
-  public DataBindingContext createBindings() {
-    if (dbc != null) {
-      dbc.dispose();
     }
-    dbc = new DataBindingContext();
-    return createBindings(dbc);
-  }
-
-  /**
-   * Creates bindings in the given {@link DataBindingContext}
-   *
-   * @param dbc the DataBindingContext for the bindings
-   */
-  public DataBindingContext createBindings(final DataBindingContext dbc) {
-    if (dbc == null) {
-      return null;
+    if (availableProjectName != null) {
+      return ResourcesPlugin.getWorkspace().getRoot().getProject(availableProjectName);
     }
-
-    final IConverter<String, IProject> convertStringToProject = IConverter.create(String.class,
-        IProject.class, projectName -> {
-          if (projectName == null || "".equals(projectName)) { //$NON-NLS-1$
-            return null;
-          }
-          // check if there is an ABAP project which matches the entered name
-          final IProject[] abapProjects = ProjectUtil.getAbapProjects();
-          String availableProjectName = null;
-          for (final IProject project : abapProjects) {
-            if (project.getName().equalsIgnoreCase(projectName)) {
-              availableProjectName = project.getName();
-              break;
-            }
-          }
-          if (availableProjectName != null) {
-            return ResourcesPlugin.getWorkspace().getRoot().getProject(availableProjectName);
-          }
-          return null;
-        });
-    final UpdateValueStrategy<IProject, String> targetUpdateStrategy = UpdateValueStrategy.create(
-        IConverter.create(IProject.class, String.class, project -> project != null ? project
-            .getName() : ""));
-    final UpdateValueStrategy<String, IProject> modelUpdateStrategy = UpdateValueStrategy.create(
-        convertStringToProject);
-    modelUpdateStrategy.setAfterGetValidator(projectName -> {
-      if (StringUtil.isBlank(projectName)) {
-        return new Status(IStatus.ERROR, AdtBaseUIPlugin.PLUGIN_ID, IStatus.INFO,
-            Messages.ProjectInput_NoProjectEntered_xmsg, null);
-      }
-      return ValidationStatus.ok();
-    });
-    modelUpdateStrategy.setAfterConvertValidator(this::validateProject);
-
-    final IObservableValue<String> projectInputTarget = WidgetProperties.text(SWT.Modify)
-        .observe(projectField);
-    final IObservableValue<IProject> projectModel = PojoProperties.value("project", IProject.class) //$NON-NLS-1$
-        .observe(projectProvider);
-    dbc.bindValue(projectInputTarget, projectModel, modelUpdateStrategy, targetUpdateStrategy);
-    return dbc;
+    return null;
   }
 
-  /**
-   * Adds the given validator to the list of project validators
-   *
-   * @param validator validator which is to be used for project validation
-   */
-  public void addProjectValidator(final IValidator<IProject> validator) {
-    projectValidators.add(validator);
+  private void fireStatusChange(IStatus projectStatus) {
+    for (IStatusChangeListener l : statusChangeListeners) {
+      l.statusChanged(projectStatus);
+    }
   }
 
   /*
    * Validates the given project
    */
-  private IStatus validateProject(final IProject project) {
+  private IStatus validateProject(final String projectName) {
+    project = null;
+    if (StringUtil.isBlank(projectName)) {
+      return new Status(IStatus.ERROR, AdtBaseUIPlugin.PLUGIN_ID,
+          Messages.ProjectInput_NoProjectEntered_xmsg, null);
+    }
+    project = convertToProject(projectName);
     if (project == null) {
       // project does not exist
-      return ValidationStatus.error(Messages.ProjectInput_ProjectDoesNotExist_xmsg);
+      return new Status(IStatus.ERROR, AdtBaseUIPlugin.PLUGIN_ID,
+          Messages.ProjectInput_ProjectDoesNotExist_xmsg, null);
+    }
+    if (this.ensureLoggedOn) {
+      IStatus loggedOnStatus = ProjectUtil.ensureLoggedOnToProject(project);
+      if (!loggedOnStatus.isOK()) {
+        project = null;
+        return loggedOnStatus;
+      }
     }
     for (final IValidator<IProject> validator : projectValidators) {
       final IStatus validationStatus = validator.validate(project);
       if (!validationStatus.isOK()) {
+        project = null;
         return validationStatus;
       }
     }
-    return ValidationStatus.ok();
-  }
 
+    return Status.OK_STATUS;
+  }
 }
