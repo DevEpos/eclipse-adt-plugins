8ee54a13d33cc280d9f1e996a1226520f478e06d
stockbal <stocki87@gmail.com>
2023-04-15 18:14:28 +0200

    feat: add method for updating the page completion status

--- com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/wizard/AbstractBaseWizardPage.java
+++ com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/wizard/AbstractBaseWizardPage.java
@@ -1,5 +1,6 @@
 package com.devepos.adt.base.ui.wizard;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.wizard.WizardPage;
 
 /**
@@ -25,4 +26,31 @@ public abstract class AbstractBaseWizardPage extends WizardPage implements IBase
     this.isDirty = isDirty;
   }
 
+  /**
+   * Updates the page completion indicator depending on the given status object
+   * 
+   * @param pageStatus the status of a page validation
+   */
+  protected void updatePageCompletedStatus(final IStatus pageStatus) {
+    boolean pageComplete = true;
+    if (pageStatus == null || pageStatus.isOK()) {
+      setErrorMessage(null);
+      setMessage(null);
+    } else if (pageStatus.getSeverity() == IStatus.ERROR) {
+      if (pageStatus.getCode() == IStatus.INFO) {
+        setErrorMessage(null);
+        setMessage(pageStatus.getMessage(), INFORMATION);
+      } else {
+        setErrorMessage(pageStatus.getMessage());
+      }
+      pageComplete = false;
+    } else if (pageStatus.getSeverity() == IStatus.WARNING) {
+      setErrorMessage(null);
+      setMessage(pageStatus.getMessage(), WARNING);
+    } else if (pageStatus.getSeverity() == IStatus.INFO) {
+      setErrorMessage(null);
+      setMessage(pageStatus.getMessage(), INFORMATION);
+    }
+    setPageComplete(pageComplete);
+  }
 }
