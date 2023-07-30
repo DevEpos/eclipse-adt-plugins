0b8f83b9dd42f71dddae30018f2020a3a197af13
stockbal <stocki87@gmail.com>
2023-07-30 20:09:05 +0200

    feat(core): enables handler from page book views as well

--- core/com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/internal/handlers/ToggleTextFilterHandler.java
+++ core/com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/internal/handlers/ToggleTextFilterHandler.java
@@ -5,6 +5,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.PageBookView;
 
 import com.devepos.adt.base.ui.tree.IFilterableView;
 
@@ -20,10 +21,24 @@ public class ToggleTextFilterHandler extends AbstractHandler {
   @Override
   public Object execute(final ExecutionEvent event) throws ExecutionException {
     IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
-    if (activePart == null || !(activePart instanceof IFilterableView)) {
+    if (activePart == null) {
       return null;
     }
-    ((IFilterableView) activePart).toggleInlineFilter();
+    IFilterableView filterableView = null;
+    if (!(activePart instanceof IFilterableView)) {
+      // check if page book view
+      if (activePart instanceof PageBookView) {
+        var page = ((PageBookView) activePart).getCurrentPage();
+        if (page != null && page instanceof IFilterableView) {
+          filterableView = (IFilterableView) page;
+        }
+      }
+    } else {
+      filterableView = (IFilterableView) activePart;
+    }
+    if (filterableView != null) {
+      filterableView.toggleTextFilterVisibility();
+    }
     return null;
   }
 
