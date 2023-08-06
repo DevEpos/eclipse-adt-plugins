62a43d6e49a9892b9780927a13969053d6889904
stockbal <stocki87@gmail.com>
2023-08-06 12:24:45 +0200

    feat(core): added adapter for IQueryListener interface

---
+++ core/com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/search/QueryListenerAdapter.java
@@ -0,0 +1,24 @@
+package com.devepos.adt.base.ui.search;
+
+import org.eclipse.search.ui.IQueryListener;
+import org.eclipse.search.ui.ISearchQuery;
+
+public class QueryListenerAdapter implements IQueryListener {
+
+  @Override
+  public void queryAdded(ISearchQuery query) {
+  }
+
+  @Override
+  public void queryRemoved(ISearchQuery query) {
+  }
+
+  @Override
+  public void queryStarting(ISearchQuery query) {
+  }
+
+  @Override
+  public void queryFinished(ISearchQuery query) {
+  }
+
+}
