a96e5b78f51a8157969e0720ab544115eed40481
stockbal <stocki87@gmail.com>
2023-08-12 15:23:46 +0200

    feat(core): add custom search query interface to allow access to destination id

---
+++ core/com.devepos.adt.base.ui/src/com/devepos/adt/base/ui/search/IAbapProjectSearchQuery.java
@@ -0,0 +1,15 @@
+package com.devepos.adt.base.ui.search;
+
+import org.eclipse.search.ui.ISearchQuery;
+
+/**
+ * {@link ISearchQuery} that searches content in a given ABAP project
+ * 
+ * @author Ludwig Stockbauer-Muhr
+ */
+public interface IAbapProjectSearchQuery extends ISearchQuery {
+  /**
+   * @return destination id of the currently used ABAP project
+   */
+  String getDestinationId();
+}
