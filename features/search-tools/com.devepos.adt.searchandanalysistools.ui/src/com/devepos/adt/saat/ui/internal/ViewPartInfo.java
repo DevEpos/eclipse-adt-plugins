cef1dd54f4f2ea08c3ba1707afd5bdf72fbd1571
Ludwig Stockbauer-Muhr <stocki87@gmail.com>
2023-09-10 21:36:05 +0200

    feat(saat): updates welcome text of CDS Analyzer view

--- features/search-tools/com.devepos.adt.searchandanalysistools.ui/src/com/devepos/adt/saat/ui/internal/ViewPartInfo.java
+++ features/search-tools/com.devepos.adt.searchandanalysistools.ui/src/com/devepos/adt/saat/ui/internal/ViewPartInfo.java
@@ -1,25 +1,18 @@
 package com.devepos.adt.saat.ui.internal;
 
-import org.eclipse.jface.resource.JFaceColors;
-import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.Bullet;
 import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.custom.StyleRange;
-import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.events.ControlEvent;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.GlyphMetrics;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Link;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.forms.events.IHyperlinkListener;
+import org.eclipse.ui.forms.widgets.FormText;
+import org.eclipse.ui.forms.widgets.FormToolkit;
 
 /**
  * Composite that can be used to display some information about e.g. Views
@@ -27,120 +20,63 @@ import org.eclipse.ui.PlatformUI;
  * @author stockbal
  */
 public abstract class ViewPartInfo extends ScrolledComposite {
-  private static final String HELP_LINK = "showHelp";
+  private static final String HELP_LINK = "showHelp"; //$NON-NLS-1$
   protected static final int BINDING_TEXT_LEFT_MARGIN = 10;
   protected static final int BULLET_LINE_INDENT = 10;
-  private Composite content;
-  private final String title;
-
-  /**
-   * Creates new info text composite
-   *
-   * @param parent the parent composite
-   */
-  public ViewPartInfo(final Composite parent) {
-    this(parent, null);
-  }
+  private FormToolkit toolkit;
+  private FormText partInfoText;
 
   /**
    * Creates new info text composite with the given title
    *
    * @param parent the parent composite
-   * @param title  the title to be used
    */
-  public ViewPartInfo(final Composite parent, final String title) {
-    super(parent, SWT.V_SCROLL | SWT.H_SCROLL);
-    this.title = title;
+  public ViewPartInfo(final Composite parent) {
+    super(parent, SWT.V_SCROLL);
     /*
      * hack to fix background problem of StyledText in Dark mode (see
      * com.sap.adt.util.ui.controls.AdtViewTextPage)
      */
-    setData("org.eclipse.e4.ui.css.id", "AdtViewTextPage");
+    // setData("org.eclipse.e4.ui.css.id", "AdtViewTextPage");
     init();
     createInformationText();
   }
 
-  /**
-   * Creates a simple Label and adds it the information text with the given text.
-   *
-   * @param text the text for the label
-   * @return
-   */
-  protected Label addLabel(final String text) {
-    final Label label = new Label(content, SWT.LEFT | SWT.TOP | SWT.WRAP);
-    label.setText(text);
-    label.setBackground(JFaceColors.getBannerBackground(Display.getDefault()));
-    return label;
+  @Override
+  public boolean setFocus() {
+    return partInfoText.forceFocus();
   }
-
-  /**
-   * Creates a section title and adds it to the information text with the given
-   * text.
-   *
-   * @param text the created styled section title
-   * @return
-   */
-  protected StyledText addSectionTitleStyledText(final String text) {
-    final StyledText styledText = addStyledText(text);
-    styledText.setFont(JFaceResources.getFont("org.eclipse.jface.bannerfont")); //$NON-NLS-1$
-    return styledText;
-  }
-
-  /**
-   * Creates a read only styled text with the given text and adds it to the
-   * information text.
-   *
-   * @param text the string to be used in the styled text
-   * @return
-   */
-  protected StyledText addStyledText(final String text) {
-    final StyledText styledText = new StyledText(content, SWT.WRAP);
-    styledText.setText(text);
-    styledText.setEditable(false);
-    return styledText;
-  }
-
-  /**
-   * Creates the contents of the information text
-   */
-
-  /**
-   * Creates a styled title with the given text and adds it to the information
-   * text.
-   *
-   * @param text the title text to be used
-   * @return
-   */
-  protected StyledText addTitleStyledText(final String text) {
-    final StyledText styledText = addStyledText(text);
-    styledText.setFont(JFaceResources.getFont("org.eclipse.jface.headerfont")); //$NON-NLS-1$
-    return styledText;
-  }
-
-  /**
-   * Subclasses must override to create the actual content.
-   */
-  protected abstract void createContent();
 
   protected void createInformationText() {
-    content = new Composite(this, SWT.NONE);
-    content.setBackground(JFaceColors.getBannerBackground(Display.getDefault()));
-    content.setLayout(new GridLayout());
-    addTitle();
+    toolkit = new FormToolkit(Display.getDefault());
+    toolkit.adapt(this);
+
+    setLayout(new GridLayout(1, true));
+    GridDataFactory.fillDefaults().grab(true, true).applyTo(this);
+
+    var content = toolkit.createComposite(this, SWT.NONE);
+    content.setLayout(new GridLayout(1, true));
+    GridDataFactory.fillDefaults().grab(true, true).applyTo(content);
+
+    partInfoText = toolkit.createFormText(content, true);
+    GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(partInfoText);
+
     createContent();
-    addHelpLink();
+    setAdditionalInfo();
     setExpandHorizontal(true);
     setExpandVertical(true);
-    this.setMinSize(50, 50);
+    setMinSize(50, 50);
     setContent(content);
     addControlListener(new ControlAdapter() {
       @Override
       public void controlResized(final ControlEvent e) {
-        final Rectangle r = ViewPartInfo.this.getClientArea();
-        ViewPartInfo.this.setMinSize(content.computeSize(r.width, -1));
+        final var area = ViewPartInfo.this.getClientArea();
+        ViewPartInfo.this.setMinSize(content.computeSize(area.width, -1));
       }
     });
   }
+
+  protected abstract String getAdditionalText();
 
   /**
    * The help context id to be used during calling the help. If it is not supplied
@@ -163,79 +99,71 @@ public abstract class ViewPartInfo extends ScrolledComposite {
     return null;
   }
 
+  protected abstract String getTitle();
+
   /**
    * Some initialization before the information content is created. <br>
    * Subclasses may override
    */
   protected void init() {
-
   }
 
   /**
-   * Sets bullet points to the list of styled texts
-   *
-   * @param texts an array of styled texts for which bullet points should be added
+   * Sets additional info for the part info
+   * Subclass may override
    */
-  protected void setLineBulletToStyledTexts(final StyledText... texts) {
-    if (texts == null) {
-      return;
-    }
-    for (final StyledText text : texts) {
-      final StyleRange styleRange = new StyleRange();
-      styleRange.metrics = new GlyphMetrics(0, 0, 10);
-      final Bullet bullet = new Bullet(styleRange);
-      text.setLeftMargin(10);
-      text.setLineBullet(0, 1, bullet);
-      text.setLineWrapIndent(0, 1, 10);
-    }
+  protected void setAdditionalInfo() {
   }
 
   /**
-   * Sets the layout data to "wrapping" for the given list of composites
-   *
-   * @param composites an array of composites
+   * Sets image for an {@code img} tag
+   * 
+   * @param hrefId href of the {@code img} tag
+   * @param image  the image to set
    */
-  protected void setWrappingLayoutData(final Composite... composites) {
-    if (composites == null) {
-      return;
-    }
-    for (final Composite composite : composites) {
-      composite.setLayoutData(new GridData(SWT.H_SCROLL | SWT.V_SCROLL));
-    }
+  protected void setImage(String hrefId, Image image) {
+    partInfoText.setImage(hrefId, image);
+  }
+
+  protected void addHyperlinkListener(IHyperlinkListener l) {
+    partInfoText.addHyperlinkListener(l);
   }
 
   /*
    * Adds help link
    */
-  private void addHelpLink() {
+  private void addHelpLink(StringBuffer body) {
     final String helpLinkText = getHelpLinkText();
     if (helpLinkText == null) {
       return;
     }
-    final Link link = new Link(content, SWT.NONE);
-    link.setBackground(JFaceColors.getBannerBackground(Display.getDefault()));
-    link.setText(String.format("<a href=\"%s\">%s %s</a>", ViewPartInfo.HELP_LINK,
+    body.append(String.format("<p><a href=\"%s\">%s %s</a></p>", ViewPartInfo.HELP_LINK, //$NON-NLS-1$
         "Learn more about", helpLinkText));
-    link.addSelectionListener(new SelectionAdapter() {
-      @Override
-      public void widgetSelected(final SelectionEvent e) {
-        if (ViewPartInfo.HELP_LINK.equals(e.text)) {
-          ViewPartInfo.this.displayHelp();
-        }
-      }
-    });
-    link.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, true, false, 1, 1));
   }
 
-  /*
-   * Adds a styled title to the info text if one was supplied
+  /**
+   * Subclasses must override to create the actual content.
    */
-  private void addTitle() {
-    if (title == null) {
-      return;
-    }
-    final StyledText title = addTitleStyledText(this.title);
-    setWrappingLayoutData(title);
+  private void createContent() {
+    var body = new StringBuffer();
+
+    body.append("<form>");//$NON-NLS-1$
+    body.append("<p><b>");//$NON-NLS-1$
+    body.append(getTitle());
+    body.append("</b></p>");//$NON-NLS-1$
+    body.append(getAdditionalText());
+
+    addHelpLink(body);
+
+    body.append("</form>"); //$NON-NLS-1$
+
+    partInfoText.setText(body.toString(), true, false);
+
+    partInfoText.addHyperlinkListener(IHyperlinkListener.linkActivatedAdapter(t -> {
+      if (t.getHref().equals(HELP_LINK)) {
+        displayHelp();
+      }
+    }));
   }
 
   private void displayDynamicHelp() {
--- features/search-tools/com.devepos.adt.searchandanalysistools.ui/src/com/devepos/adt/saat/ui/internal/cdsanalysis/view/CdsAnalysisWelcomeText.java
+++ features/search-tools/com.devepos.adt.searchandanalysistools.ui/src/com/devepos/adt/saat/ui/internal/cdsanalysis/view/CdsAnalysisWelcomeText.java
@@ -1,12 +1,23 @@
 package com.devepos.adt.saat.ui.internal.cdsanalysis.view;
 
-import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.widgets.Composite;
+import java.util.Arrays;
+import java.util.Collections;
 
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.forms.events.IHyperlinkListener;
+import org.eclipse.ui.handlers.IHandlerService;
+
+import com.devepos.adt.saat.ui.internal.SearchAndAnalysisPlugin;
 import com.devepos.adt.saat.ui.internal.ViewPartInfo;
+import com.devepos.adt.saat.ui.internal.handlers.RunCdsAnalysisHandler;
 import com.devepos.adt.saat.ui.internal.help.HelpContexts;
 import com.devepos.adt.saat.ui.internal.help.HelpUtil;
 import com.devepos.adt.saat.ui.internal.messages.Messages;
+import com.devepos.adt.saat.ui.internal.util.IImages;
 
 /**
  * Info text for CDS Analysis View
@@ -14,23 +25,27 @@ import com.devepos.adt.saat.ui.internal.messages.Messages;
  * @author stockbal
  */
 public class CdsAnalysisWelcomeText extends ViewPartInfo {
+  private static final String RUN_NEW_ANALYSIS_HREF = "RUN_ANALYSIS"; //$NON-NLS-1$
 
   public CdsAnalysisWelcomeText(final Composite parent) {
     super(parent);
   }
 
   @Override
-  protected void createContent() {
-    final StyledText startingInfo = addSectionTitleStyledText(
-        Messages.CdsAnalysis_NoOpenCdsAnalysis_xmsg);
-    final StyledText option1 = addStyledText(Messages.CdsAnalysisWelcome_Option1_xmsg);
-    final StyledText option2 = addStyledText(Messages.CdsAnalysisWelcome_Option2_xmsg);
-    final StyledText option3 = addStyledText(Messages.CdsAnalysisWelcome_Option3_xmsg);
-    final StyledText option4 = addStyledText(Messages.CdsAnalysisWelcome_Option4_xmsg);
-    final StyledText option5 = addStyledText(Messages.CdsAnalysisWelcome_Option5_xmsg);
+  protected String getAdditionalText() {
+    var additional = new StringBuffer();
 
-    setWrappingLayoutData(startingInfo, option1, option2, option3, option4, option5);
-    setLineBulletToStyledTexts(option1, option2, option3, option4, option5);
+    addRunAnalysisActionLink(Messages.CdsAnalysisWelcome_Option1_xmsg, additional);
+    addRunAnalysisActionLink(Messages.CdsAnalysisWelcome_Option2_xmsg, additional);
+
+    for (var option : Arrays.asList(Messages.CdsAnalysisWelcome_Option3_xmsg,
+        Messages.CdsAnalysisWelcome_Option4_xmsg, Messages.CdsAnalysisWelcome_Option4_xmsg,
+        Messages.CdsAnalysisWelcome_Option5_xmsg)) {
+      additional.append("<li>"); //$NON-NLS-1$
+      additional.append(option);
+      additional.append("</li>"); //$NON-NLS-1$
+    }
+    return additional.toString();
   }
 
   @Override
@@ -43,4 +58,33 @@ public class CdsAnalysisWelcomeText extends ViewPartInfo {
     return Messages.CdsAnalysisWelcome_HelpSuffix_xlnk;
   }
 
+  @Override
+  protected String getTitle() {
+    return Messages.CdsAnalysis_NoOpenCdsAnalysis_xmsg;
+  }
+
+  @Override
+  protected void setAdditionalInfo() {
+    setImage(RUN_NEW_ANALYSIS_HREF, SearchAndAnalysisPlugin.getDefault()
+        .getImage(IImages.RUN_NEW_ANALYSIS));
+    addHyperlinkListener(IHyperlinkListener.linkActivatedAdapter(t -> {
+      if (t.getHref().equals(RUN_NEW_ANALYSIS_HREF)) {
+        IEvaluationContext context = ((IHandlerService) PlatformUI.getWorkbench()
+            .<IHandlerService>getService(IHandlerService.class)).getCurrentState();
+        try {
+          new RunCdsAnalysisHandler().execute(new ExecutionEvent(null, Collections.emptyMap(), null,
+              context));
+        } catch (ExecutionException x) {
+          x.printStackTrace();
+        }
+      }
+    }));
+  }
+
+  private void addRunAnalysisActionLink(String textBeforeActionLink, StringBuffer buffer) {
+    buffer.append(String.format("<li>%s <img align=\"middle\" href=\"%s\"/> <a href=\"%s\">", //$NON-NLS-1$
+        textBeforeActionLink, RUN_NEW_ANALYSIS_HREF, RUN_NEW_ANALYSIS_HREF));
+    buffer.append(Messages.CdsAnalysisWelcomeText_runNewCdsAnalysisCommand_xlbl);
+    buffer.append("</a></li>"); //$NON-NLS-1$
+  }
 }
