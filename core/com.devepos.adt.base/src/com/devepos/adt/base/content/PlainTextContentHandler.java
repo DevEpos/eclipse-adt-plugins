282a4d9be903c685d37ad4fa3a0386b593d97b12
Ludwig Stockbauer-Muhr <stocki87@gmail.com>
2023-10-05 02:31:46 +0200

    fix(core): writes received content 'as is'

--- core/com.devepos.adt.base/src/com/devepos/adt/base/content/PlainTextContentHandler.java
+++ core/com.devepos.adt.base/src/com/devepos/adt/base/content/PlainTextContentHandler.java
@@ -1,13 +1,11 @@
 package com.devepos.adt.base.content;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.ByteArrayOutputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 import com.sap.adt.communication.content.AdtMediaType;
+import com.sap.adt.communication.content.ContentHandlerException;
 import com.sap.adt.communication.content.IContentHandler;
 import com.sap.adt.communication.message.ByteArrayMessageBody;
 import com.sap.adt.communication.message.IMessageBody;
@@ -22,20 +20,14 @@ public class PlainTextContentHandler implements IContentHandler<String> {
   @Override
   public String deserialize(final IMessageBody body, final Class<? extends String> clazz) {
     try {
-      final InputStream is = body.getContent();
-      final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-      final StringBuilder out = new StringBuilder();
-      String line;
-      while ((line = reader.readLine()) != null) {
-        out.append(line);
-      }
-      reader.close();
-      return out.toString();
-    } catch (final IOException e) {
-      e.printStackTrace();
-    }
+      var bos = new ByteArrayOutputStream((int) body.getContentLength());
+      body.writeTo(bos);
 
-    return null;
+      return (String) clazz.getConstructor(byte[].class, Charset.class)
+          .newInstance(bos.toByteArray(), StandardCharsets.UTF_8);
+    } catch (final Exception e) {
+      throw new ContentHandlerException(e.getMessage());
+    }
   }
 
   @Override
