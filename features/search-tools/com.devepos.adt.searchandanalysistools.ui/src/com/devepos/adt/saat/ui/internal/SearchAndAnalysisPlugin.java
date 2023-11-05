e96802ddb713066a3dc8fb00df8012229dc514c9
Ludwig Stockbauer-Muhr <stocki87@gmail.com>
2023-11-05 18:48:54 +0100

    refactor(saat): remove redundant icons

--- features/search-tools/com.devepos.adt.searchandanalysistools.ui/src/com/devepos/adt/saat/ui/internal/SearchAndAnalysisPlugin.java
+++ features/search-tools/com.devepos.adt.searchandanalysistools.ui/src/com/devepos/adt/saat/ui/internal/SearchAndAnalysisPlugin.java
@@ -89,36 +89,14 @@ public class SearchAndAnalysisPlugin extends AbstractAdtUIPlugin {
   protected void initializeImageRegistry(final ImageRegistry imageRegistry) {
     // register all kinds of images
     registerImage(imageRegistry, IImages.DB_BROWSER_DATA_PREVIEW, "icons/DBBrowser.png");
-    registerImage(imageRegistry, IImages.ADT_DATA_DATA_PREVIEW, "icons/ADTDataPreview.png");
     registerImage(imageRegistry, IImages.CDS_VIEW, "icons/CDSEntity.png");
-    registerImage(imageRegistry, IImages.TABLE_DEFINITION, "icons/Table.png");
     registerImage(imageRegistry, IImages.VIEW_DEFINITION, "icons/ViewDefinition.png");
-    registerImage(imageRegistry, IImages.SEARCH_HISTORY, "icons/full/elcl16/search_history.png",
-        "org.eclipse.search");
     registerImage(imageRegistry, IImages.HISTORY_LIST, "icons/HistoryList.png");
-    registerImage(imageRegistry, IImages.USER, "icons/UserEdit.png");
-    registerImage(imageRegistry, IImages.PROPERTIES, "icons/Properties.png");
-    registerImage(imageRegistry, IImages.DATE, "icons/Date.png");
-    registerImage(imageRegistry, IImages.CLASS, "icons/Class.png");
-    registerImage(imageRegistry, IImages.INTERFACE, "icons/Interface.png");
-    registerImage(imageRegistry, IImages.CLASS_INTERFACE, "icons/ClassInterface.png");
-    registerImage(imageRegistry, IImages.RELEASED, "icons/Success.png");
-    registerImage(imageRegistry, IImages.NOT_RELEASED, "icons/Error.png");
-    registerImage(imageRegistry, IImages.FOLDER, "icons/Folder.png");
-    registerImage(imageRegistry, IImages.VIRTUAL_FOLDER, "icons/VirtualFolder.png");
-    registerImage(imageRegistry, IImages.TECHNICAL_SETTINGS, "icons/TechnicalSettings.png");
-    registerImage(imageRegistry, IImages.ACCESS_CONTROL, "icons/AccessControl.png");
-    registerImage(imageRegistry, IImages.BUSINESS_OBJECT, "icons/BusinessObject.png");
-    registerImage(imageRegistry, IImages.METADATA_EXTENSION, "icons/MetadataExtension.png");
     registerImage(imageRegistry, IImages.EXCEL_APPLICATION, "icons/Excel.png");
     registerImage(imageRegistry, IImages.ANALYTICAL_QUERY, "icons/AnalyticalQuery.png");
     registerImage(imageRegistry, IImages.EXTERNAL_TOOLS, "icons/ExternalTools.png");
-    registerImage(imageRegistry, IImages.SAP_GUI_LOGO, "icons/SapGuiLogo.png");
-    registerImage(imageRegistry, IImages.INNER_JOIN, "icons/InnerJoin.png");
     registerImage(imageRegistry, IImages.OBJECT_SEARCH, "icons/ABAPSearch.png");
     registerImage(imageRegistry, IImages.CDS_ANALYZER, "icons/CdsAnalyzerView.png");
-    registerImage(imageRegistry, IImages.USED_ENTITES, "icons/UsedEntities.png");
-    registerImage(imageRegistry, IImages.USED_IN_FROM, "icons/UsedInFrom.png");
     registerImage(imageRegistry, IImages.TOP_DOWN, "icons/TopDown.png");
     registerImage(imageRegistry, IImages.FIELD_ANALYSIS, "icons/CdsFieldAnalysis.png");
     registerImage(imageRegistry, IImages.WHERE_USED_IN, "icons/WhereUsedInCds.png");
@@ -130,14 +108,7 @@ public class SearchAndAnalysisPlugin extends AbstractAdtUIPlugin {
     registerImage(imageRegistry, IImages.KEY_COLUMN, "icons/KeyColumn.png");
     registerImage(imageRegistry, IImages.FIELD_TOP_DOWN, "icons/FieldTopDown.png");
     registerImage(imageRegistry, IImages.FIELD_WHERE_USED, "icons/FieldWhereUsed.png");
-    registerImage(imageRegistry, IImages.CALCULATOR, "icons/Calculator.png");
     registerImage(imageRegistry, IImages.FUNCTION, "icons/Function.png");
-    registerImage(imageRegistry, IImages.ABAP_VERSION, "icons/Version.gif");
-    registerImage(imageRegistry, IImages.SUPER_TYPE, "icons/SuperType.png");
-    registerImage(imageRegistry, IImages.FRIEND, "icons/Friend.png");
-    registerImage(imageRegistry, IImages.ATTRIBUTE, "icons/Attribute.png");
-    registerImage(imageRegistry, IImages.METHOD, "icons/Method.png");
-    registerImage(imageRegistry, IImages.ENABLED_CHECKBOX, "icons/EnabledCheckbox.png");
 
     // register overlay images
     registerImage(imageRegistry, IImages.RELEASED_API_OVR, "icons/ovr/Released.png");
@@ -153,21 +124,12 @@ public class SearchAndAnalysisPlugin extends AbstractAdtUIPlugin {
         "icons/ovr/CustomEntity.png");
 
     // Object Search parameter image registrations
-    registerImage(imageRegistry, IImages.DESCRIPTION_PARAM, "icons/Documentation.png");
-    registerImage(imageRegistry, IImages.PARAMETER_PARAM, "icons/ImportingParameter.png");
     registerImage(imageRegistry, IImages.PACKAGE_PARAM, "icons/obj/package_obj.png",
         "com.sap.adt.tools.core.ui");
-    registerImage(imageRegistry, IImages.USER_PARAM, "icons/User.png");
-    registerImage(imageRegistry, IImages.API_PARAM, "icons/APIState.png");
     registerImage(imageRegistry, IImages.SELECT_FROM_PARAM, "icons/SelectFromSource.png");
-    registerImage(imageRegistry, IImages.FIELD_PARAM, "icons/Column.png");
-    registerImage(imageRegistry, IImages.ANNOTATION_PARAM, "icons/Annotation.png");
+    registerImage(imageRegistry, IImages.COLUMN, "icons/Column.png");
     registerImage(imageRegistry, IImages.USED_AS_ASSOCICATION_PARAM,
         "icons/AssociationDeclaration.png");
-    registerImage(imageRegistry, IImages.MAX_ROWS_PARAM, "icons/Count.png");
-    registerImage(imageRegistry, IImages.TYPE_PARAM, "icons/TypeFolder.png");
-    registerImage(imageRegistry, IImages.ABAP_TYPE, "icons/TypeGroup.png");
-    registerImage(imageRegistry, IImages.EXTENSION, "icons/ExtensionPoint.png");
     registerImage(imageRegistry, IImages.RUN_NEW_ANALYSIS, "icons/RunCdsAnalysis.png");
   }
 }
--- features/search-tools/com.devepos.adt.searchandanalysistools.ui/src/com/devepos/adt/saat/ui/internal/util/IImages.java
+++ features/search-tools/com.devepos.adt.searchandanalysistools.ui/src/com/devepos/adt/saat/ui/internal/util/IImages.java
@@ -7,10 +7,6 @@ package com.devepos.adt.saat.ui.internal.util;
  */
 public interface IImages {
   /**
-   * Identifies the Image for the ADT Data Preview
-   */
-  String ADT_DATA_DATA_PREVIEW = "IMG_ADT_DATA_PREVIEW";
-  /**
    * Identifies the Image for the DB Browser Data Preview
    */
   String DB_BROWSER_DATA_PREVIEW = "IMG_DATA_PREVIEW";
@@ -19,41 +15,13 @@ public interface IImages {
    */
   String CDS_VIEW = "IMG_CDS_VIEW";
   /**
-   * Identifies the image for a Database table definition
-   */
-  String TABLE_DEFINITION = "IMG_TABLE";
-  /**
    * Identifies the image for a Database view
    */
   String VIEW_DEFINITION = "IMG_SELECT";
   /**
-   * Identifies image for class
-   */
-  String CLASS = "IMG_CLASS";
-  /**
-   * Identifies image for interface
-   */
-  String INTERFACE = "IMG_INTERFACE";
-  /**
-   * Identifies image for class/interface
-   */
-  String CLASS_INTERFACE = "IMG_CLASS_INTF";
-  /**
    * Icon for a list of history entries
    */
   String HISTORY_LIST = "IMG_HISTORY_LIST";
-  /**
-   * Identifies the image for the Search history
-   */
-  String SEARCH_HISTORY = "IMG_SEARCH_HISTORY";
-  /**
-   * Identifies the image for the user parameter
-   */
-  String USER_PARAM = "IMG_USER";
-  /**
-   * Identifies the image for a SAP User Logon
-   */
-  String USER = "IMG_USER_EDIT";
   /**
    * Identifies the image for the API Release state parameter
    */
@@ -71,10 +39,6 @@ public interface IImages {
    */
   String PACKAGE_PARAM = "IMG_PACKAGE";
   /**
-   * Identifies the image for the Field parameter
-   */
-  String FIELD_PARAM = "IMG_COLUMN";
-  /**
    * Identifies the image for a column/field
    */
   String COLUMN = "IMG_COLUMN";
@@ -83,10 +47,6 @@ public interface IImages {
    */
   String KEY_COLUMN = "IMG_KEY_COLUMN";
   /**
-   * Identifies the image for the annotation parameter
-   */
-  String ANNOTATION_PARAM = "IMG_ANNOTATION";
-  /**
    * Identifies the image for the <strong>Used as association</strong> parameter
    */
   String USED_AS_ASSOCICATION_PARAM = "IMG_ASSOCIATION";
@@ -94,78 +54,6 @@ public interface IImages {
    * Identifies an image for a CDS association
    */
   String ASSOCIATION = USED_AS_ASSOCICATION_PARAM;
-  /**
-   * Identifies the image for the Description parameter
-   */
-  String DESCRIPTION_PARAM = "IMG_DOCU";
-  /**
-   * Identifies the image for the <em>Parameter</em> parameter
-   */
-  String PARAMETER_PARAM = "IMG_PARAMETER";
-  /**
-   * Identifies the image for the <em>Max rows</em> parameter
-   */
-  String MAX_ROWS_PARAM = "IMG_NUMBER";
-  /**
-   * Identifies the iamge for the <em>type</em> parameter
-   */
-  String TYPE_PARAM = "IMG_TYPE";
-  /**
-   * Identifies an image for a type folder
-   */
-  String TYPE_GROUP = "IMG_TYPE";
-  /**
-   * Identifies the <em>type</em> image
-   */
-  String ABAP_TYPE = "IMG_ABAP_TYPE";
-  /**
-   * Identifies an image for the <em>Extended by</em> parameter
-   */
-  String EXTENSION = "IMG_EXTENSION";
-  /**
-   * Identifies an image for an extension view
-   */
-  String EXTENSION_VIEW = CDS_VIEW;
-  /**
-   * Identifies an image for properties
-   */
-  String PROPERTIES = "IMG_PROPERTIES";
-  /**
-   * Identifies an image for Date
-   */
-  String DATE = "IMG_DATE";
-  /**
-   * Identifies an image to signal that something is not released
-   */
-  String NOT_RELEASED = "IMG_ERROR";
-  /**
-   * Identifies an image to signal that something is released
-   */
-  String RELEASED = "IMG_SUCCESS";
-  /**
-   * Folder Image
-   */
-  String FOLDER = "IMG_FOLDER";
-  /**
-   * Virtual Folder Image
-   */
-  String VIRTUAL_FOLDER = "IMG_VIRTUAL_FOLDER";
-  /**
-   * Technical Table Settings image
-   */
-  String TECHNICAL_SETTINGS = "IMG_TECH_SETTINGS";
-  /**
-   * Image for an ABAP Access Control File
-   */
-  String ACCESS_CONTROL = "IMG_ACCESS_CONTROl";
-  /**
-   * Image for an ABAP DDLS Metadata Extension file
-   */
-  String METADATA_EXTENSION = "IMG_METADATA_EXT";
-  /**
-   * Image for an ABAP Business Object
-   */
-  String BUSINESS_OBJECT = "IMG_BOBF";
   /**
    * Image for Excel
    */
@@ -179,14 +67,6 @@ public interface IImages {
    */
   String EXTERNAL_TOOLS = "IMG_EXTERNAL_TOOLS";
   /**
-   * Image for SAP GUI Logo
-   */
-  String SAP_GUI_LOGO = "IMG_SAP_GUI";
-  /**
-   * Image for SQL inner join condition
-   */
-  String INNER_JOIN = "IMG_INNER_JOIN";
-  /**
    * Image for the object search
    */
   String OBJECT_SEARCH = "IMG_OBJECT_SEARCH";
@@ -194,14 +74,6 @@ public interface IImages {
    * Image for Cds Hierarchy view
    */
   String CDS_ANALYZER = "IMG_HIERARCHY";
-  /**
-   * Image for Entities that were used in from clause of other CDS views
-   */
-  String USED_IN_FROM = "IMG_USED_IN_FROM";
-  /**
-   * Image for entities used in from clause of current CDS
-   */
-  String USED_ENTITES = "IMG_USED_ENTITIES";
   /**
    * Image for CDS usage analyzer
    */
@@ -221,7 +93,7 @@ public interface IImages {
   /**
    * Image for Select part of SQL
    */
-  String SELECT_PART = "IMG_SELECT";
+  String SELECT_PART = VIEW_DEFINITION;
   /**
    * Image for a Join Result of a SQL Select
    */
@@ -275,10 +147,6 @@ public interface IImages {
    */
   String FIELD_ANALYSIS = "IMG_FIELD_ANALYSIS";
   /**
-   * Image to mark elements which were calculated
-   */
-  String CALCULATOR = "IMG_CALCULATOR";
-  /**
    * Image to mark elements which are functional
    */
   String FUNCTION = "IMG_FUNCTION";
@@ -286,30 +154,6 @@ public interface IImages {
    * Image id for "Package"
    */
   String PACKAGE = PACKAGE_PARAM;
-  /**
-   * Image id for "Version"
-   */
-  String ABAP_VERSION = "IMG_VERSION";
-  /**
-   * Images id for "Global Friend"
-   */
-  String FRIEND = "IMG_FRIEND";
-  /**
-   * Image id for "Super type"
-   */
-  String SUPER_TYPE = "IMG_SUPER_TYPE";
-  /**
-   * Image id for "Method"
-   */
-  String METHOD = "IMG_METHOD";
-  /**
-   * Image id for "Attribute"
-   */
-  String ATTRIBUTE = "IMG_ATTRIBUTE";
-  /**
-   * Image id for "Enabled Checkbox"
-   */
-  String ENABLED_CHECKBOX = "IMG_ENABLED_CHECKBOX";
   /**
    * Image id for running a new CDS analysis
    */
