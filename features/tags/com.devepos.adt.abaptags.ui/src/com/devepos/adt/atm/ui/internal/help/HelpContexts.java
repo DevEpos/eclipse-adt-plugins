f49a063da6cc80ee9a6ef080f9f826e5a18df5a8
stockbal <stocki87@gmail.com>
2025-06-16 13:43:53 +0200

    fix(tags): fixes context id referenced in the HelpContexts class

--- features/tags/com.devepos.adt.abaptags.ui/src/com/devepos/adt/atm/ui/internal/help/HelpContexts.java
+++ features/tags/com.devepos.adt.abaptags.ui/src/com/devepos/adt/atm/ui/internal/help/HelpContexts.java
@@ -16,7 +16,7 @@ public enum HelpContexts {
   IMPORT_TAGS_SOURCE_SELECTION("import_tags_wiz_source_sel"),
   IMPORT_TAGS_CONTENT_SELECTION("import_tags_wiz_content_sel"),
   EXPORT_TAGS_WIZARD("export_tags_wizard"),
-  EXPORT_TAGS_WIZARD_CONTENT_SEL("export_tags_wiz_sel"),
+  EXPORT_TAGS_WIZARD_CONTENT_SEL("export_tags_wizard_content_sel"),
   TAG_MANAGER("tag_manager"),
   OBJECT_TAGS("object_tags"),
   TAGGED_OBJECT_SEARCH("tagged_object_search");
