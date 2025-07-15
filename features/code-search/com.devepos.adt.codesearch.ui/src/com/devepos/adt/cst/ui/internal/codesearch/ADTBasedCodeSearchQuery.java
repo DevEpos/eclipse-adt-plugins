0d8d5413d6b4ef7624653a8582c05a8e266b6b79
stockbal <stocki87@gmail.com>
2025-07-15 14:15:58 +0200

    fix(codesearch): fixes work progress + minor clean-up

--- features/code-search/com.devepos.adt.codesearch.ui/src/com/devepos/adt/cst/ui/internal/codesearch/ADTBasedCodeSearchQuery.java
+++ features/code-search/com.devepos.adt.codesearch.ui/src/com/devepos/adt/cst/ui/internal/codesearch/ADTBasedCodeSearchQuery.java
@@ -6,7 +6,6 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,11 +17,6 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubMonitor;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 
 import com.devepos.adt.base.content.PlainTextContentHandler;
 import com.devepos.adt.base.destinations.DestinationUtil;
@@ -61,10 +55,12 @@ public class ADTBasedCodeSearchQuery extends CodeSearchQuery {
     final var session = AdtSystemSessionFactory.createSystemSessionFactory()
         .createStatelessSession(destination);
     try {
+      // monitor.beginTask("Loading scope for code search...", 1);
       var service = AdtRisVirtualFoldersServiceFactory
           .createVirtualFoldersSearchService(destination);
+      monitor.beginTask("Determine objects", 1);
       folderContent = service.getContent(params, monitor);
-
+      monitor.worked(1);
     } catch (ServiceNotAvailableException e) {
       e.printStackTrace();
     } catch (IOException e) {
@@ -81,14 +77,11 @@ public class ADTBasedCodeSearchQuery extends CodeSearchQuery {
 
     ExecutorService executor = Executors.newFixedThreadPool(numJobs);
     List<Future<IStatus>> futures = new ArrayList<>();
-    SubMonitor subMonitor = SubMonitor.convert(monitor, "", folderContent.getObjectCount());
+    monitor.beginTask("Searching", folderContent.getObjectCount());
 
     try {
       for (var chunk : chunks) {
-        // var workLoad = chunk.size() / numJobs;
-        var workLoad = chunk.size();
-        futures.add(
-            executor.submit(() -> runSearchTask(subMonitor.split(workLoad), destination, chunk)));
+        futures.add(executor.submit(() -> runSearchTask(monitor, destination, chunk)));
       }
 
       for (Future<IStatus> future : futures) {
@@ -113,46 +106,6 @@ public class ADTBasedCodeSearchQuery extends CodeSearchQuery {
 
   }
 
-  private void getFolderObjectsForPackage(String destination) {
-    /*
-     * System.out.println("Duration of content loading and search: " +
-     * (System.nanoTime() - startTime) / 1_000_000_000.0 + "s");
-     * System.out.println(
-     * "Duration of string search: " + (searchDuration.get()) / 1_000_000_000.0 + "s");
-     * System.out.println("Found matches: " + matchCount.get());
-     */
-
-    var readVirtualFolderContents = Job.create("Loading Source Content", (m) -> {
-      var params = AdtRisVirtualFoldersServiceFactory.createVirtualFolderRequestParameters();
-      params.setObjectSearchPattern("*");
-      params.addPreselection("package", "/AIF/STRUC");
-      params.addPreselection("type", "CLAS");
-      // params.setWantedFacets(Arrays.asList("type"));
-      final var session = AdtSystemSessionFactory.createSystemSessionFactory()
-          .createStatelessSession(destination);
-      try {
-        var service = AdtRisVirtualFoldersServiceFactory
-            .createVirtualFoldersSearchService(destination);
-        folderContent = service.getContent(params, m);
-        m.done();
-      } catch (ServiceNotAvailableException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    });
-
-    readVirtualFolderContents.addJobChangeListener(new JobChangeAdapter() {
-
-      @Override
-      public void done(IJobChangeEvent event) {
-        ADTBasedCodeSearchQuery.this.runParallelJobs(destination);
-      }
-
-    });
-    readVirtualFolderContents.schedule();
-  }
-
   private static <T> List<List<T>> splitList(List<T> list, int chunkSize) {
     List<List<T>> chunks = new ArrayList<>();
     for (int i = 0; i < list.size(); i += chunkSize) {
@@ -163,7 +116,7 @@ public class ADTBasedCodeSearchQuery extends CodeSearchQuery {
 
   private IStatus runSearchTask(IProgressMonitor m, String destination, List<IObject> chunk) {
     try {
-      m.beginTask("Searching code chunk with size ", chunk.size());
+      // m.beginTask("Searching code chunk with size ", chunk.size());
       final var session = AdtSystemSessionFactory.createSystemSessionFactory()
           .createStatelessSession(destination);
       for (var o : chunk) {
@@ -186,90 +139,8 @@ public class ADTBasedCodeSearchQuery extends CodeSearchQuery {
         m.worked(1);
       }
     } finally {
-      m.done();
     }
     return Status.OK_STATUS;
   }
 
-  private void runParallelJobs(String destination) {
-    int numJobs = 10;
-    System.out.println("Processing folder with " + folderContent.getObjectCount() + " classes");
-    var chunks = splitList(folderContent.getObject(),
-        (int) Math.ceil((double) folderContent.getObjectCount() / numJobs));
-    var countdownLatch = new CountDownLatch(chunks.size());
-    var matchCount = new AtomicInteger();
-
-    var jobCount = 0;
-
-    for (var chunk : chunks) {
-      var job = new Job("Processing Chunk") {
-        @Override
-        protected IStatus run(IProgressMonitor m) {
-          try {
-            System.out.println("Started new chunk job with obj count: " + chunk.size());
-            final var session = AdtSystemSessionFactory.createSystemSessionFactory()
-                .createStatelessSession(destination);
-            m.beginTask("Loading Chunk with Size ", chunk.size());
-            for (var o : chunk) {
-              // var searchDuration = new AtomicLong(0L);
-              var pattern = Pattern.compile("DATA");
-              var getClassContent = URI.create("/sap/bc/adt/oo/classes/" +
-                  URLEncoder.encode(o.getName(), StandardCharsets.UTF_8) + "/source/main");
-              var resource = AdtRestResourceFactory.createRestResourceFactory()
-                  .createRestResource(getClassContent, session);
-              resource.addContentHandler(new PlainTextContentHandler());
-              IHeaders.IField headerField = HeadersFactory.newField("Accept",
-                  new String[] { "text/plain" });
-              IHeaders requestHeaders = HeadersFactory.newHeaders();
-              requestHeaders.addField(headerField);
-              var source = resource.get(m, requestHeaders, String.class);
-              final var matcher = pattern.matcher(source);
-              while (matcher.find()) {
-                matchCount.set(matchCount.get() + 1);
-              }
-              m.worked(1);
-            }
-          } finally {
-            countdownLatch.countDown();
-          }
-          return Status.OK_STATUS;
-        }
-      };
-
-      long queue = jobCount++ % numJobs;
-      var parallelExecutionRule = new NumberedQueueSchedulingRule(queue);
-      job.setRule(parallelExecutionRule);
-      job.schedule();
-    }
-
-    try {
-      var startTime = System.nanoTime();
-      countdownLatch.await();
-      System.out.println(
-          "Content search completed in " + (System.nanoTime() - startTime) / 1_000_000_000.0 + "s");
-    } catch (InterruptedException exc) {
-      Thread.currentThread().interrupt();
-    }
-  }
-
-}
-
-class NumberedQueueSchedulingRule implements ISchedulingRule {
-  private long queueNumber;
-
-  public NumberedQueueSchedulingRule(long queueNumber) {
-    this.queueNumber = queueNumber;
-  }
-
-  public boolean contains(ISchedulingRule rule) {
-    return rule == this;
-  }
-
-  public boolean isConflicting(ISchedulingRule rule) {
-    if (rule instanceof NumberedQueueSchedulingRule) {
-      var otherRule = (NumberedQueueSchedulingRule) rule;
-      return otherRule.queueNumber == queueNumber;
-    }
-    return false;
-  }
 }
